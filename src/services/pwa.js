6e2967e9d9bb42e85f433676b10f6355a3bf842b
satyamjh07 <138577860+satyamjh07@users.noreply.github.com>
2026-03-29 21:35:22 +0530

    Delete src directory

--- src/services/pwa.js
+++
@@ -1,9 +0,0 @@
-export function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(() => {
-        // silent fail in local/dev unsupported contexts
-      });
-    });
-  }
-}
